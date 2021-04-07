182ce5e998330e6ce321d0a5a19279de2cb49bb9
Predator HackerzZ <77227205+P-Phreak@users.noreply.github.com>
2021-04-07 17:18:56 +0530

    script.py

--- script.py
+++ script.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# @trojanzhex
+# @teamteleroid
 
 
 class Script(object):
 
-    START_MSG = """<b>Hello {},
+    START_MSG = """<b>Hey! {},
 
-I can extract audio and subtitles from video files.
+This is a bot to extract audio and subtitles from video files.
 
 Send me any video file and extract what you want :).
 
 See <i>help</i> for more details.
 
-© @TroJanzHEX</b>"""
+© @TeleRoid14</b>"""
 
 
-    HELP_MSG = """<b>Hai, Follow these Steps..</b>
+    HELP_MSG = """<b>Oye, Follow these Steps....</b>
 
 🌀 <i>Send me any Valid video file.</i>
 
@@ -26,15 +26,17 @@ See <i>help</i> for more details.
 
 🌀 <i>Now select the stream you need to extract!</i>
 
-<b>© @TroJanzHEX</b>"""
+<b>© @TheTeleRoid</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Streams Extractor Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creator :</b> @TheTeleRoid
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
+ 
+⭕<b>Source  :</b> <a href='https://github.com/TeamTeleRoid/Streams-Extractor
 
 """
