d2da30c44451cf35d2d8a45282b5ba812666bca1
Predator HackerzZ <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-06-19 10:33:46 +0530

    extractor.py
    Making bot for Public Use

--- plugins/extractor.py
+++ plugins/extractor.py
@@ -14,7 +14,6 @@ from script import Script
 @trojanz.on_message(filters.private & (filters.document | filters.video))
 async def confirm_dwnld(client, message):
 
-    if message.from_user.id not in Config.AUTH_USERS:
 
     media = message
     filetype = media.document or media.video
